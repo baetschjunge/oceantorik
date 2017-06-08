508745986697fdd106b8c877cbfeca02b24f465d
ban jamien <baetschjunge@googlemail.com>
2017-06-09 00:15:06 +0200

    add init base project

---
+++ bundle.config.js
@@ -0,0 +1,47 @@
+// bundle.config.js
+module.exports = {
+    bundle: {
+        'js/header': {
+            scripts: [
+                //{
+                //    src: 'node_modules/slick-carousel/slick/slick.js',
+                //    minSrc: 'node_modules/slick-carousel/slick/slick.min.js'
+                //},
+            ],
+            options: {
+                useMin: true,
+                uglify: false, // {(boolean|string|Array)} js minification
+                rev: false
+            }
+        },
+        'js/main': {
+            scripts: [
+                {
+                    src: 'node_modules/jquery/dist/jquery.js',
+                    minSrc: 'node_modules/jquery/dist/jquery.min.js'
+                },
+                //'./source/js/*js'
+                {
+                    src: 'source/assets/js/*js',
+                },
+            ],
+            styles: [
+
+            ],
+            options: {
+                useMin: true,
+                uglify: false, // {(boolean|string|Array)} js minification
+                rev: false
+            }
+        },
+
+    },
+    copy: [
+        {
+            base: './source/assets',
+            src: [
+                './source/assets/img/**/*.{jpg,png,svg}'
+            ]
+        }
+    ]
+};
---
+++ source/assets/js/navigation-header-btn-search.js
@@ -0,0 +1,47 @@
+/*
+ *   navigation-header-btn-search (Toggle) for search-input and search-event
+ *   created by b.b.
+ *
+ */
+
+ // TODO there is still a bug left with the event.target.id
+
+
+(function ($) {
+    $(document).ready(function () {
+
+        // Search-Input-Field of Search-Bar
+        var searchInput = $('.header-nav__mid__search .search__form__input');
+        var searchButton = $('.header-nav__mid__search .search__form__submit');
+
+
+        // close Search-Input-Field by clicking anywhere else on the page
+        function removeSearchForm(event) {
+            if (event.target.id != "search-form-js") {
+
+                // TODO goes here
+
+                //console.log(event.target.id);
+                searchInput.removeClass('is-open');
+            }
+        }
+        $('body').bind('click', removeSearchForm);
+
+        // open Search-Input-Field by clicking Search-Icon, if its closed
+        // process search, if its open
+        searchButton.click(function (event) {
+            // just checking for "is-open" does not suffice
+            // since this class is not evaluated for mobile screens
+            if (searchInput.css("visibility") == "hidden") {
+                event.preventDefault(event);
+                event.stopPropagation();
+
+                searchInput.addClass('is-open');
+                // wait for button to be focused
+                setTimeout(function () {
+                    searchInput.focus();
+                }, 300);
+            }
+        });
+    });
+})(jQuery);
