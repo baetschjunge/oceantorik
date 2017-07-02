d74a20af92b2e139b95dc0b62065fc98eacc1a16
ban jamien <baetschjunge@googlemail.com>
2017-07-02 15:55:52 +0200

    add some lil magic to search

--- source/assets/js/navigation-header-btn-search.js
+++ source/assets/js/navigation-header-btn-search.js
@@ -7,41 +7,47 @@
  // TODO there is still a bug left with the event.target.id
 
 //
-// (function ($) {
-//     $(document).ready(function () {
-//
-//         // Search-Input-Field of Search-Bar
-//         var searchInput = $('.header-nav__mid__search .search__form__input');
-//         var searchButton = $('.header-nav__mid__search .search__form__submit');
-//
-//
-//         // close Search-Input-Field by clicking anywhere else on the page
-//         function removeSearchForm(event) {
-//             if (event.target.id != "search-form-js") {
-//
-//                 // TODO goes here
-//
-//                 //console.log(event.target.id);
-//                 searchInput.removeClass('is-open');
-//             }
-//         }
-//         $('body').bind('click', removeSearchForm);
-//
-//         // open Search-Input-Field by clicking Search-Icon, if its closed
-//         // process search, if its open
-//         searchButton.click(function (event) {
-//             // just checking for "is-open" does not suffice
-//             // since this class is not evaluated for mobile screens
-//             if (searchInput.css("visibility") == "hidden") {
-//                 event.preventDefault(event);
-//                 event.stopPropagation();
-//
-//                 searchInput.addClass('is-open');
-//                 // wait for button to be focused
-//                 setTimeout(function () {
-//                     searchInput.focus();
-//                 }, 300);
-//             }
-//         });
-//     });
-// })(jQuery);
+(function ($) {
+    $(document).ready(function () {
+
+        var searchComp = $('#search-js');
+        var searchInput = $('#search-js .search__input');
+        var searchButton = $('#search-js .search__button');
+
+        searchButton.click(function (event) {
+          searchInput.toggleClass('search__input--closed');
+        });
+
+
+        // open Search-Input-Field by clicking Search-Icon, if its closed
+        // process search, if its open
+        // searchButton.click(function (event) {
+        //   console.log("search yo");
+        //     // just checking for "is-open" does not suffice
+        //     // since this class is not evaluated for mobile screens
+        //     // if (searchInput.css("visibility") == "hidden") {
+        //     //     event.preventDefault(event);
+        //     //     event.stopPropagation();
+        //
+        //   searchInput.removeClass('.search__input--closed');
+        //   // wait for button to be focused
+        //   setTimeout(function () {
+        //       searchInput.focus();
+        //   }, 300);
+        //     // }
+
+        // close Search-Input-Field by clicking anywhere else on the page
+        // function removeSearchForm(event) {
+        //   console.log(event.target.id);
+        //     if (event.target.id != "search-js") {
+        //
+        //         // TODO goes here
+        //
+        //         //console.log(event.target.id);
+        //         searchInput.addClass('search__input--closed');
+        //     }
+        // }
+        // $('body').bind('click', removeSearchForm);
+
+    });
+})(jQuery);
